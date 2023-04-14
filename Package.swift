a5101d25b6c0eda4b5242f7d9c344f14d93978cd
koy.kw <koy.kw@kakaomobility.com>
2023-04-14 10:15:26 +0900

    [Add] KNSDK-UI 1.8.0

--- Package.swift
+++ Package.swift
@@ -1,10 +1,9 @@
 // swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let sdkName = "KNSDK"
-let version = "1.6.7"
+let version = "1.8.0"
 
 let package = Package(
     name: sdkName,
@@ -17,16 +16,24 @@ let package = Package(
             targets: ["KNSDKBundle"])
     ],
     dependencies: [
-        .package(url: "https://github.com/kakaomob/KMLocationSDK.git", exact: Version(stringLiteral: "1.2.23")),
+        .package(url: "https://github.com/kakaomob/KMLocationSDK.git", exact: Version(stringLiteral: "1.3.17")),
     ],
     targets: [
         .binaryTarget(name: sdkName,
-                      url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/\(version)/KNSDK-UI/KNSDK.xcframework.zip",
-                      checksum: "47c25f0153ad7d5af496315ed079da6223276a55b2777f4c37bd898662c25240"),
+                      url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/1.8.0-beta.1/KNSDK-UI/KNSDK.xcframework.zip",
+                      checksum: "c425b14175b3c71ae75af22b798dd3a0919e136eb7bcf6eddc46152691ba4ee6"),
+        .binaryTarget(name: "Realm",
+                      url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/1.8.0-beta.1/Realm/Realm.xcframework.zip",
+                      checksum: "38d347d80465ad936f3770c1a732db99cdf4673b6c75179757e02a8a0d95868d"),
+        .binaryTarget(name: "RealmSwift",
+                      url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/1.8.0-beta.1/Realm/RealmSwift.xcframework.zip",
+                      checksum: "cb8cafcd32ef80e55e3455f27d06dacb079c5c2fae4d8869737c4f291ffc0a41"),
         .target(name: "KNSDKBundle",
                 dependencies: [
                     .target(name: sdkName),
-                    .product(name: "LocationFramework", package: "KMLocationSDK")
+                    .product(name: "KMLocationSDK", package: "KMLocationSDK"),
+                    .target(name: "Realm"),
+                    .target(name: "RealmSwift")
                 ]
         )
     ],
