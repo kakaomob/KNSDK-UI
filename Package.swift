55b5746fcf942535fc265d5a06164bee1a87040b
koy.kw <koy.kw@kakaomobility.com>
2026-01-09 14:24:33 +0900

    - KNSDK 1.12.15

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let sdkName = "KNSDK"
-let version = "1.12.14"
+let version = "1.12.15"
 
 let package = Package(
     name: sdkName,
@@ -33,6 +33,7 @@ func getRealmFromNexus() -> [Target] {
                     .product(name: "KMLocationSDK", package: "KMLocationSDK"),
                     .target(name: "Realm"),
                     .target(name: "RealmSwift"),
+                    .target(name: "KNSDKCore"),
                 ],
                 resources: [
                     .process("PrivacyInfo.xcprivacy")
@@ -49,6 +50,9 @@ func getRealmFromNexus() -> [Target] {
         .binaryTarget(name: "RealmSwift",
                       url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/Realm/10.54.6/26.0.1/RealmSwift.xcframework.zip",
                       checksum: "ba5e0a74bf6fc64097b769df9f19a36b2b6434059324c5ff9037411ca0782576"),
+        .binaryTarget(name: "KNSDKCore",
+                        url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/KNSDKCore/1.0.0/KNSDKCore.xcframework.zip",
+                        checksum: "6f6d43d72a8ab58c216389b9f00176250d420806539e7d45ed8f3dd29959f522"),
     ]
     #elseif compiler(>=6.1.2)
     // 16.4
@@ -59,6 +63,9 @@ func getRealmFromNexus() -> [Target] {
         .binaryTarget(name: "RealmSwift",
                       url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/Realm/10.54.5/16.4/RealmSwift.xcframework.zip",
                       checksum: "7bc9e9ee16a457c49c4f772ceafd4435a73fc15799eb67311d2eb5bcdc2518c3"),
+        .binaryTarget(name: "KNSDKCore",
+                        url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/KNSDKCore/1.0.0/KNSDKCore.xcframework.zip",
+                        checksum: "6f6d43d72a8ab58c216389b9f00176250d420806539e7d45ed8f3dd29959f522"),
         ]
     
     #elseif compiler(>=6.1.0)
@@ -70,6 +77,9 @@ func getRealmFromNexus() -> [Target] {
         .binaryTarget(name: "RealmSwift",
                       url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/Realm/10.54.5/16.3/RealmSwift.xcframework.zip",
                       checksum: "5726c1e2e33d3dd0ce4583424517fb5feb73b43b3d4b9cc6c2d258e6ce3f2f50"),
+        .binaryTarget(name: "KNSDKCore",
+                        url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/KNSDKCore/1.0.0/KNSDKCore.xcframework.zip",
+                        checksum: "6f6d43d72a8ab58c216389b9f00176250d420806539e7d45ed8f3dd29959f522"),
         ]
     #elseif compiler(>=6.0.3)
     // 16.2
@@ -80,6 +90,9 @@ func getRealmFromNexus() -> [Target] {
         .binaryTarget(name: "RealmSwift",
                       url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/Realm/10.54.5/16.2/RealmSwift.xcframework.zip",
                       checksum: "2d03d25c8b52914a3c3bbc64184b66b8ce3d93f7a7c4b0ef0af300805827ce61"),
+        .binaryTarget(name: "KNSDKCore",
+                        url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/KNSDKCore/1.0.0/KNSDKCore.xcframework.zip",
+                        checksum: "6f6d43d72a8ab58c216389b9f00176250d420806539e7d45ed8f3dd29959f522"),
         ]
     #elseif compiler(>=5.10.0)
     // 15.4
@@ -90,6 +103,9 @@ func getRealmFromNexus() -> [Target] {
         .binaryTarget(name: "RealmSwift",
                       url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/Realm/10.54.5/15.4/RealmSwift.xcframework.zip",
                       checksum: "a06b55cfd4b08cd82fae28b5cebc628baf1a1d238bd9a1c2c308e52a5b1587bc"),
+        .binaryTarget(name: "KNSDKCore",
+                        url: "https://devrepo.kakaomobility.com/repository/kakao-mobility-ios-knsdk-release/KNSDKCore/1.0.0/KNSDKCore.xcframework.zip",
+                        checksum: "6f6d43d72a8ab58c216389b9f00176250d420806539e7d45ed8f3dd29959f522"),
         ]
     #else
         return []
